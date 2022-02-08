907d03ba120c83c1395f888cdeb17f660550c964
Jivendra <jivendrasah33@gmail.com>
2022-02-08 19:26:02 +0530

    _

---
+++ C++/temp.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout<<endl;
+
+    cout << int('\0');
+    int x = 0x100;
+    cout<<endl<<x<<" "<<sizeof(long);
+    cout<<endl;
+    return 0;
+}
